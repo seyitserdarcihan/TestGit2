518afc98d1235504408413e36e5fe2329f3ab7e8
Seyit Serdar Cihan <seyit.s.cihan.1987@gmail.com>
2023-05-06 20:02:07 +0300

    Versiyon 1

---
+++ src/main/java/Class001.java
@@ -0,0 +1,12 @@
+public class Class001 {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+        int y = 2;
+        System.out.println(x+y);
+        System.out.println("Versiyon 1 gerçekleştirildi");
+
+
+    }
+}
